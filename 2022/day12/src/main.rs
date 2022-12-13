01c32c96e074e8a8fabf737ae56a88779490d731
jinlow <james.d.inlow@gmail.com>
2022-12-13 11:57:12 -0600

    Finish day12

--- 2022/day12/src/main.rs
+++ 2022/day12/src/main.rs
@@ -1,5 +1,5 @@
 use std::cmp::Reverse;
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{BinaryHeap, HashMap};
 use std::error::Error;
 use std::fs;
 
@@ -43,50 +43,29 @@ impl Node {
 
 impl Ord for Node {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.steps_to_node
-            .cmp(&other.steps_to_node)
-            .then_with(|| self.key.cmp(&other.key))
+        self.steps_to_node.cmp(&other.steps_to_node)
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let file = fs::read_to_string("input.txt")?;
-    let mut elevation = HashMap::<(usize, usize), usize>::new();
-    let mut start = (0, 0);
-    let mut end = (0, 0);
-    let mut graph_max = (0, 0);
-    for (i, l) in file.lines().enumerate() {
-        graph_max.0 = std::cmp::max(graph_max.0, i);
-        for (j, c) in l.bytes().enumerate() {
-            graph_max.1 = std::cmp::max(graph_max.1, j);
-            let v = match c {
-                b'S' => {
-                    start = (i, j);
-                    calc_weight(b'a')
-                }
-                b'E' => {
-                    end = (i, j);
-                    calc_weight(b'z')
-                }
-                _ => calc_weight(c),
-            };
-            elevation.insert((i, j), v);
-        }
-    }
-    // println!("start: {:?}, end: {:?}", start, end);
+fn dijkstra(
+    start: (usize, usize),
+    end: (usize, usize),
+    elevation: &HashMap<(usize, usize), usize>,
+) -> Option<usize> {
+    let i_max = *elevation.keys().map(|(i, _)| i).max().unwrap();
+    let j_max = *elevation.keys().map(|(_, j)| j).max().unwrap();
 
     let mut shortest_paths = HashMap::<(usize, usize), usize>::new();
     let mut heap = BinaryHeap::new();
     heap.push(Reverse(Node::new(start, 0)));
     shortest_paths.insert(start, 0);
-    let mut previous_node = HashMap::new();
     while !heap.is_empty() {
         let node = match heap.pop() {
             Some(Reverse(x)) => x,
             None => break,
         };
-        let neighbors = get_neighbors(&node.key, &graph_max);
-        // println!("{:?}, {:?}", node, neighbors);
+        let neighbors = get_neighbors(&node.key, &(i_max, j_max));
+
         for n in neighbors {
             // Is this a valid neighbor...
             let node_e = elevation.get(&node.key).unwrap();
@@ -100,11 +79,51 @@ fn main() -> Result<(), Box<dyn Error>> {
             if new_shortest_path < neighbor_shortest_path {
                 heap.push(Reverse(Node::new(n, neighbor_shortest_path)));
                 shortest_paths.insert(n, new_shortest_path);
-                previous_node.insert(n, node.key);
             }
         }
     }
-    // println!("{:?}", shortest_paths);
-    println!("{}", shortest_paths.get(&end).unwrap());
+    shortest_paths.get(&end).copied()
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let file = fs::read_to_string("input.txt")?;
+    let mut elevation = HashMap::<(usize, usize), usize>::new();
+    let mut start = (0, 0);
+    let mut end = (0, 0);
+    let mut graph_max = (0, 0);
+    let mut a_nodes = Vec::new();
+    for (i, l) in file.lines().enumerate() {
+        graph_max.0 = std::cmp::max(graph_max.0, i);
+        for (j, c) in l.bytes().enumerate() {
+            graph_max.1 = std::cmp::max(graph_max.1, j);
+            let v = match c {
+                b'S' => {
+                    start = (i, j);
+                    a_nodes.push(start);
+                    calc_weight(b'a')
+                }
+                b'E' => {
+                    end = (i, j);
+                    calc_weight(b'z')
+                }
+                b'a' => {
+                    a_nodes.push((i, j));
+                    calc_weight(c)
+                }
+                _ => calc_weight(c),
+            };
+            elevation.insert((i, j), v);
+        }
+    }
+    // P1
+    println!("{}", dijkstra(start, end, &elevation).unwrap());
+
+    // P2
+    let all_a = a_nodes
+        .iter()
+        .map(|s| dijkstra(*s, end, &elevation).unwrap_or(usize::MAX))
+        .collect::<Vec<usize>>();
+
+    println!("{}", all_a.iter().min().unwrap());
     Ok(())
 }
