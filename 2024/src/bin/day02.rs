2379f2ca3012eb6ac2bc887c8a2bdb72886201ed
jinlow <james.d.inlow@gmail.com>
2025-01-19 13:27:51 -0600

    day 2 - 2024

--- 2024/src/bin/day02.rs
+++ 2024/src/bin/day02.rs
@@ -1,3 +1,44 @@
-fn main() {
-    println!("Is this working day 2?");
+use std::error::Error;
+use std::fs;
+
+fn is_safe(v: &[i64]) -> bool {
+    let mut sign = (v[0] - v[1]).signum();
+    for x in v.windows(2) {
+        let diff = x[0] - x[1];
+        let nsign = diff.signum();
+        if (sign != nsign) || (diff.abs() > 3) || (diff.abs() == 0) {
+            return false;
+        }
+        sign = nsign;
+    }
+    true
+}
+
+fn is_safe_tollerant(v: &[i64]) -> bool {
+    if is_safe(v) {
+        return true
+    } else {
+        for i in 0..v.len() {
+            let mut vc = v.to_vec();
+            vc.remove(i);
+            if is_safe(&vc) {
+                return true
+            }
+        }
+    }
+    false
+}
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let i = fs::read_to_string("input/day02.txt")?
+        .lines()
+        .map(|l| l.split(" ").map(|n| n.parse().unwrap()).collect())
+        .collect::<Vec<Vec<i64>>>();
+    let nsafe: i64 = i.iter().map(|v| is_safe(v) as i64).sum();
+    println!("{}", nsafe);
+
+    let tol_nsafe: i64 = i.iter().map(|v| is_safe_tollerant(v) as i64).sum();
+    println!("{}", tol_nsafe);
+    Ok(())
 }
